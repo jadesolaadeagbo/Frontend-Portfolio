cd464b49ea114e4ccd89d3f630aae7caab4da531
Adeagbo Jadesola Rubies <jadesolaadeagbo@gmail.com>
2022-11-18 23:00:02 +0100

    feat: Navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Logo from "../../assets/Logo.svg"
+import "./Navbar.css"
+import searchNormal from "../../assets/searchNormal.svg"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={Logo} alt="" />
+
+      <div className='container'>
+        <ul className='nav'>
+            <li>Home</li>
+            <li>Teams <i class="fa fa-caret-down "></i></li>
+            <li>FAQs</li>
+            <li>About HNG</li>
+        </ul>
+
+        <div className="search-div">
+        <form className='search-form'>
+            <img src={searchNormal} alt="" />
+            <input type="text" className='search-input' placeholder='Search about HNG, teams and mentors'/>
+        </form>
+        </div>
+            
+        
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
